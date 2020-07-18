666425e18083af8d9c685e92957b3c61846a0fd6
Vitor Conroy <64869691+str4vinsk@users.noreply.github.com>
2020-07-18 17:12:29 -0300

    Update digiscon.bat

--- digiscon.bat
+++ digiscon.bat
@@ -2,11 +2,11 @@
 @chcp 65001
 
 set ext=%~x1
+set nome=%~n1
 
 IF EXIST "%1" (
 
 	IF "%ext%" == ".txt" (
-		set nome=%~n1
 		java -jar encoder.jar -i "%1" -o "%nome%".bin -l resources/br.properties
 		python2 duck2spark.py -i "%nome%".bin -o "%nome%".ino
 	) ELSE (
