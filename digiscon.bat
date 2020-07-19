517c8f7543ff7d3fb628193984cbdf898a07c0e9
Vitor Conroy <64869691+str4vinsk@users.noreply.github.com>
2020-07-19 14:58:49 -0300

    Update digiscon.bat

--- digiscon.bat
+++ digiscon.bat
@@ -7,14 +7,15 @@ set nome=%~n1
 IF EXIST "%1" (
 
 	IF "%ext%" == ".txt" (
+		ECHO [!!] Converting from duck script to a binary file...
 		java -jar encoder.jar -i "%1" -o "%nome%".bin -l resources/br.properties
 		python2 duck2spark.py -i "%nome%".bin -o "%nome%".ino
 	) ELSE (
-		ECHO O arquivo não é um txt!
+		ECHO [!!] Please, select a valid ".txt" file [!!]
 	)
 
 ) ELSE ( 
-	ECHO [!!] O arquivo indicado não existe! [!!]
-  	ECHO Por favor, selecione um arquivo ".txt" válido.	
+	ECHO [!!] File - "%1" - not found [!!]
+  	ECHO Please, select a valid ".txt" file.	
 )
 
